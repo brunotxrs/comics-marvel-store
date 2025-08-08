3836f7c6d77c9acd59f21cb7056580465560fb27
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-08 17:31:22 -0400

    feat: Creation of the Main component and its styles

--- src/modules/main/main.js
+++ src/modules/main/main.js
@@ -1,49 +1,70 @@
-import { MainContainer } from './styles';
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import md5 from 'js-md5';
+import { MainContainer, BoxInfo, H2Info, H3Info, PInfo, ButtonInfo, AInfo
+} from './styles';
+import { useFetchApiComicVine, useFetchApiComicVineBackground } from '../../services/api';
+import { useEffect, useState } from 'react';
 
 
 function Main() {
-  const [characters, setCharacters] = useState([]);
-  const PUBLIC_KEY = '16a6689cc985c2e858e4f3ed066cf981';
-  const PRIVATE_KEY = '08321f2952fc07ed52b473963ad75f2e665852bc';
- 
+  const [currentPosterIndex, setCurrentPosterIndex] = useState(0);
 
+  const characters = useFetchApiComicVine();
+  const charactersIssues = useFetchApiComicVineBackground();
 
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      try {
-        const timeStamp = new Date().getTime();
-        const stringToHash = timeStamp + PRIVATE_KEY + PUBLIC_KEY;
-
-        const HASH = md5(stringToHash);
-
-        console.log('Timestamp:', timeStamp);
-        console.log('String a ser hashed:', stringToHash);
-        console.log('Hash gerado pelo código:', HASH);
-        
-        const url = `https://gateway.marvel.com/v1/public/comics?ts=${timeStamp}&apikey=${PUBLIC_KEY}&hash=${HASH}`;
-
-         const response = await axios.get(url);
-        
-        setCharacters(response.data.data.results)
-
-
-      } catch (error) {
-        console.error("Erro ao buscar personagens:", error);
-      }
+  const posterComics = [
+    {
+      id: 1,
+      imgPoster: 'https://comic-perfect.myshopify.com/cdn/shop/files/marvel-stories_600x.webp?v=1676286937',
+      title: 'All Store',
+      subTitle: 'Confira a coleção de revistas',
+      description: 'Explore nosso catálogo completo e encontre os quadrinhos de seus heróis favoritos, edições raras e sagas inesquecíveis.',
+      textButton: 'Shop Now',
+      linkUrl: '/store/all'
+    },
+    {
+      id: 2,
+      imgPoster: 'https://comic-perfect.myshopify.com/cdn/shop/files/comic-stories_600x.webp?v=1676287116',
+      title: 'Special offers',
+      subTitle: 'Confira a coleção de revistas especiais',
+      description: 'Descubra as edições mais cobiçadas e colecionáveis com descontos exclusivos por tempo limitado!',
+      textButton: 'Ofertas Especiais',
+      linkUrl: '/store/all'
     }
+  ]
 
-    fetchCharacters();
+   useEffect(() => {
+    // Seta um intervalo para mudar o índice a cada 5 segundos
+    const interval = setInterval(() => {
+      setCurrentPosterIndex(prevIndex => (prevIndex + 1) % posterComics.length);
+    }, 5000); // 5000ms = 5 segundos
 
-  }, [])
-  
+    // Limpa o intervalo quando o componente for desmontado para evitar vazamento de memória
+    return () => clearInterval(interval);
+  }, [posterComics]); // Dependência: posterComics
+
+  // Pega o item atual a ser exibido
+  const currentPoster = posterComics[currentPosterIndex];
+
+ 
   return (
     <MainContainer>
-      {characters.map((character) => (
-          <li key={character.id}>{character.name}</li>
-        ))}
+      <BoxInfo key={currentPoster.id}>
+        <img src={currentPoster.imgPoster}/>
+        <H2Info>{currentPoster.title}</H2Info>
+        <H3Info>{currentPoster.subTitle}</H3Info>
+        <PInfo>{currentPoster.description}</PInfo>
+        <ButtonInfo>
+          <AInfo href={currentPoster.linkUrl}>{currentPoster.textButton}</AInfo>
+        </ButtonInfo>
+
+      </BoxInfo>
+
+      {/* this area for exhibition for Magazine Comics */}
+      {/* {characters.map((character) => (
+        <li key={character.id}>
+          <h2>{character.name}</h2>
+          <img src={character.image.original_url} alt={character.name} />
+        </li>
+      ))} */}
     </MainContainer>
   );
 }
--- src/modules/main/styles.js
+++ src/modules/main/styles.js
@@ -1,10 +1,72 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const MainContainer = styled.main`
-  background-image: url(${props => props.imageUrl});
-  background-size: cover;
-  background-position: center;
-  height: 100vh;
-  // Outros estilos do container
-
+const slideUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
+
+export const MainContainer = styled.main `
+  background: var(--second-color);
+`;
+
+
+export const BoxInfo = styled.div `
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 15px 30px;
+  
+`
+
+export const H2Info = styled.h2 `
+  color: var(--first-color);
+  font-size: var(--title);
+  font-family: var(--font-cabinet_grotesk);
+  margin-bottom: 15px;
+  animation: ${slideUp} 0.9s ease-out;
+`
+
+export const H3Info = styled.h3 `
+  color: var(--third-color);
+  font-size: var(--subtitle);
+  font-family: var(--font-nuckle);
+  margin-bottom: 15px;
+  animation: ${slideUp} 0.9s ease-out;
+`
+
+export const PInfo = styled.p `
+  color: var(--third-color);
+  font-size: var(--paragraph);
+  font-family: var(--font-nuckle);
+  margin-bottom: 20px;
+  line-height: 200%;
+  animation: ${slideUp} 1.0s ease-out;
+`
+
+export const ButtonInfo = styled.button `
+  outline: none;
+  border: none;
+  width: 150px;
+  height: 50px;
+  background: var(--first-color);
+  border-radius: 4px;
+  animation: ${slideUp} 1.0s ease-out;
+`
+
+export const AInfo = styled.a `
+  color: var(--second-color);
+  width: 100%;
+  height: 100%;
+  font-size: var(--title);
+  font-family: var(--font-cabinet_grotesk);
+  text-decoration: none;
+  padding: 5px 0;
+
+`
