44fe89ce66eb79e7734b33c3468a3f4d1a05f107
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-08 10:24:18 -0400

    feat: import component main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './modules/header/header';
+import Main from './modules/main/main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     
     </>
   );
