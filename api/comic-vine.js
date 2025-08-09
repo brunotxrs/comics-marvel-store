e8e7876e0c572507b4338c71d59ab149a4952672
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-09 15:24:51 -0400

    fix: Usa a API nativa fetch na função serverless

--- api/comic-vine.js
+++ api/comic-vine.js
@@ -1,16 +1,18 @@
-import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
     const { api_key, format, limit } = req.query;
-    
+
     const url = `https://comicvine.gamespot.com/api/issues/?api_key=${api_key}&format=${format}&limit=${limit}`;
     
-    const response = await axios.get(url);
-    
-    // Retorna os dados da API para o front-end
-    res.status(200).json(response.data);
+    // Usamos o fetch nativo do Node.js para evitar a dependência do axios
+    const response = await fetch(url);
 
+    // O fetch retorna a resposta em formato JSON, então precisamos converter
+    const data = await response.json();
+
+    res.status(200).json(data);
+    
   } catch (error) {
     console.error("Erro na função serverless:", error);
     res.status(500).json({ error: 'Erro ao buscar dados da API.' });
