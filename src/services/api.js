46671945d2084acc6a2395c725418e9820c5f630
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-09 17:57:35 -0400

    feat: Add logic for local development environment

--- src/services/api.js
+++ src/services/api.js
@@ -8,8 +8,11 @@ export function useFetchApiComicVine(){
         const fetchCharacters = async () => {
             try {
                 const KEY_API = '1ea7892a7a15cb41314f574fcc6277380d2c345e';
+
+                const isLocalhost = window.location.hostname === 'localhost';
+                const baseUrl = isLocalhost ? 'https://comics-marvel-store.vercel.app' : '';
                 
-                const urlServerless = `/api/comic-vine/?api_key=${KEY_API}&format=json&limit=20`;
+                const urlServerless = `${baseUrl}/api/comic-vine/?api_key=${KEY_API}&format=json&limit=20`;
 
                 // Use fetch nativo do navegador para a requisição
                 const response = await fetch(urlServerless);
@@ -24,6 +27,7 @@ export function useFetchApiComicVine(){
                 
                 console.log('Dados da API Comic Vine:', data);
                 setCharacters(data.results);
+
             } catch (error) {
                 console.error("Erro ao buscar personagens da Comic Vine:", error);   
             }
