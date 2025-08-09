b8f68dbfa742f3a04b731bb8003eb410c578028d
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-09 13:24:00 -0400

    feat: Create ModuleBanners component

---
+++ src/modules/main/box-banner/banners.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import {  useFetchApiComicVine  } from '../../../services/api'
+import {  BoxBanner, LiBanner, ImgBanner, Loader  } from '../styles'
+
+
+function ModuleBanners(){
+    const allBanners = useFetchApiComicVine();
+    const [randomBanners, setRandomBanners] = useState([]);
+
+    useEffect(() => {
+        if(allBanners.length > 0 ){
+            const shuffledBanners = [...allBanners].sort(() => 0.5 - Math.random());
+
+
+            setRandomBanners(shuffledBanners.slice(0, 2));
+        }
+    }, [allBanners])
+
+    return (
+        <>
+
+        <BoxBanner>
+            {randomBanners.length > 0 ? (
+                randomBanners.map((banner) => (
+                    <LiBanner key={banner.id}>
+                        <ImgBanner src={banner.image.original_url} alt={banner.name} />
+                    </LiBanner>
+                ))
+
+            ): (
+                <Loader />
+            )}
+
+
+        </BoxBanner>
+
+
+
+        </>
+
+    );
+
+}
+
+export default ModuleBanners;
+
+
