b8c6b9d1c434fc9adc4dcf1ad4f6517687ca2e89
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-08 23:11:03 -0400

    feat: Integrates modules into the Main component

--- src/modules/main/main.js
+++ src/modules/main/main.js
@@ -1,63 +1,20 @@
-import { MainContainer, BoxInfo, H2Info, H3Info, PInfo, ButtonInfo, AInfo
-} from './styles';
+import { MainContainer } from './styles';
 import { useFetchApiComicVine, useFetchApiComicVineBackground } from '../../services/api';
-import { useEffect, useState } from 'react';
-
+import ModuleBoxInfo from './box-info/box_info';
+import ModuleBoxComicShop from './box-comic-shop/comic-shop';
 
 function Main() {
-  const [currentPosterIndex, setCurrentPosterIndex] = useState(0);
 
   const characters = useFetchApiComicVine();
   const charactersIssues = useFetchApiComicVineBackground();
-
-  const posterComics = [
-    {
-      id: 1,
-      imgPoster: 'https://comic-perfect.myshopify.com/cdn/shop/files/marvel-stories_600x.webp?v=1676286937',
-      title: 'All Store',
-      subTitle: 'Confira a coleção de revistas',
-      description: 'Explore nosso catálogo completo e encontre os quadrinhos de seus heróis favoritos, edições raras e sagas inesquecíveis.',
-      textButton: 'Shop Now',
-      linkUrl: '/store/all'
-    },
-    {
-      id: 2,
-      imgPoster: 'https://comic-perfect.myshopify.com/cdn/shop/files/comic-stories_600x.webp?v=1676287116',
-      title: 'Special offers',
-      subTitle: 'Confira a coleção de revistas especiais',
-      description: 'Descubra as edições mais cobiçadas e colecionáveis com descontos exclusivos por tempo limitado!',
-      textButton: 'Ofertas Especiais',
-      linkUrl: '/store/all'
-    }
-  ]
-
-   useEffect(() => {
-    // Seta um intervalo para mudar o índice a cada 5 segundos
-    const interval = setInterval(() => {
-      setCurrentPosterIndex(prevIndex => (prevIndex + 1) % posterComics.length);
-    }, 5000); // 5000ms = 5 segundos
-
-    // Limpa o intervalo quando o componente for desmontado para evitar vazamento de memória
-    return () => clearInterval(interval);
-  }, [posterComics]); // Dependência: posterComics
-
-  // Pega o item atual a ser exibido
-  const currentPoster = posterComics[currentPosterIndex];
-
  
   return (
     <MainContainer>
-      <BoxInfo key={currentPoster.id}>
-        <img src={currentPoster.imgPoster}/>
-        <H2Info>{currentPoster.title}</H2Info>
-        <H3Info>{currentPoster.subTitle}</H3Info>
-        <PInfo>{currentPoster.description}</PInfo>
-        <ButtonInfo>
-          <AInfo href={currentPoster.linkUrl}>{currentPoster.textButton}</AInfo>
-        </ButtonInfo>
+      <ModuleBoxInfo />
 
-      </BoxInfo>
-
+      {/* benefits of ComicShop */}
+      <ModuleBoxComicShop />
+      
       {/* this area for exhibition for Magazine Comics */}
       {/* {characters.map((character) => (
         <li key={character.id}>
