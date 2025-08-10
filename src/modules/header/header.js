5fb7e94aa9be7e4ec53d3e94c3b62a8e985446ec
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-10 12:18:09 -0400

    feat: Displays the total number of items in the cart in the header

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -1,12 +1,13 @@
-import {  
-    Container, BoxLeft, 
-    Img, BoxRight, CountCart, 
-    StyleIcon  } from './styles'
-import logoCMS from '../../assets/svg/logo-comics-marvel-store.svg'
+import {  Container, BoxLeft, Img, BoxRight, CountCart, StyleIcon  } from './styles'
+import logoCMS from '../../assets/svg/logo-comics-marvel-store.svg';
 import { faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { useSelector } from 'react-redux';
 
 function Header({onCartClick}){
+    const totalItemCart = useSelector(state => state.cart.items);
+    // total de items no carrinho
+    const totalItem = totalItemCart.reduce((e, i) => (e + i.quantity), 0)
 
     return(
         <>
@@ -18,11 +19,19 @@ function Header({onCartClick}){
                 <StyleIcon icon={faUser} />
                     <Img src={logoCMS} alt="Comics Marvel Store Logo"/>
             
-
-                <BoxRight>
-                    <CountCart>0</CountCart>
-                    <StyleIcon icon={faCartShopping} onClick={onCartClick}/>
-                </BoxRight>
+                {totalItemCart.length > 0 ? (
+                    totalItemCart.map((total) => (
+                        <BoxRight key={total.id}>
+                            <CountCart>{totalItem}</CountCart>
+                            <StyleIcon icon={faCartShopping} onClick={onCartClick}/>
+                        </BoxRight>
+                    ))
+                ):(
+                    <BoxRight>
+                        <CountCart>0</CountCart>
+                        <StyleIcon icon={faCartShopping} onClick={onCartClick}/>
+                    </BoxRight>
+                )}
 
             </Container>
         </>
