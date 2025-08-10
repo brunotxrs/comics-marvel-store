99e609abc596457b1ca17e95cbdeb730bade0ffc
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-09 22:15:50 -0400

    style: Styles the layout and internal elements of the cart

--- src/modules/cart-shopping/styles.js
+++ src/modules/cart-shopping/styles.js
@@ -12,21 +12,96 @@ export const ContainerCart = styled.section  `
   z-index: 100; 
   display: flex;
   flex-direction: column;
-//   justify-content: center;
   align-items: center;
   color: var(--second-colo); 
   padding: 20px;
 `;
+
 export const BoxIconClosed = styled.span `
-    width: 100%;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: right;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: right;
 `;
 
 export const StyleIconClosed = styled(FontAwesomeIcon) `
-    width: 30px;
-    height: 30px;
-    color: var(--first-color);
+  width: 30px;
+  height: 30px;
+  color: var(--first-color);
 `;
+
+export const BoxTotalPay = styled.div `
+  outline: 1px solid #fff;
+  width: 100%;
+
+`;
+
+export const TextTotalPay = styled.h2 `
+  outline: 1px solid #fff;
+  color: #fff;
+
+`;
+
+export const BoxItems = styled.div `
+  outline: 1px solid #fff;
+
+`;
+
+export const UlItems = styled.ul `
+  outline: 1px solid orange;
+  width: 350px;
+`;
+
+export const LiItems = styled.li `
+  outline: 1px solid #fff;
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  width: 100%;
+
+`;
+
+export const SpanItems = styled.span `
+  outline: 1px solid red;
+  width: 100%;
+  height: 100%;
+
+`
+
+export const ImgItems = styled.img `
+  width: 100%;
+  outline: 1px solid red;
+
+  `
+
+export const DivIconsItems = styled.div `
+  display: flex;
+  flex-direction: row;
+  height: 50px;
+  align-items: center;
+  width: 50%;
+  outline: 1px solid;
+  justify-content: space-evenly;
+`
+export const StyleIconMinus = styled(FontAwesomeIcon) `
+  width: 20px;
+  height: 20px;
+  color: var(--first-color);
+`
+
+export const StyleCount = styled.p `
+  width: 20px;
+  height: 20px;
+  color: var(--second-color);
+`
+
+export const StyleIconMore = styled(FontAwesomeIcon) `
+  width: 20px;
+  height: 20px;
+  color: var(--first-color);
+`
+
+
+
+
