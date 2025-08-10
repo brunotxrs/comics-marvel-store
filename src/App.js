7d9b2686d9ec39dd703c57889dadb43494486ce3
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-09 20:44:54 -0400

    feat: Integrates logic and cart components into the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import Header from './modules/header/header';
 import Main from './modules/main/main';
+import CartShopping from './modules/cart-shopping/cart_shopping';
+
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+   const toggleCart = () => {
+    // Inverte o valor de 'isCartOpen' para abrir ou fechar o carrinho
+    setIsCartOpen(!isCartOpen);
+  };
+
+
+
   return (
     <>
-      <Header />
+      <Header onCartClick={toggleCart} />
+      {isCartOpen && <CartShopping onCloseCart={toggleCart}/>}
       <Main />
     
     </>
