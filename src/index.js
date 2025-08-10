4f0bbe1ee4020e6927ce32a94d0bfbd316630d61
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-10 00:03:14 -0400

    feat: Configures the Redux store and connects to the app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReduce from './store/cartSlice';
+
+
+const store = configureStore({
+  reducer: {
+    cart: cartReduce
+  }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
